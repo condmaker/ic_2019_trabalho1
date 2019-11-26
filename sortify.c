6eaec658db4da092f66a233ce5459aa5db752564
Marco Domingos <a21901309@alunos.ulht.pt>
2019-11-26 15:50:22 +0000

    Second commit, added part of the challenge function

--- sortify.c
+++ sortify.c
@@ -17,13 +17,14 @@
 int rand_number(const int, const int);
 void print_status(const int, const int, const int);
 void print_menu(void);
+void challenge(int, int, int);
 
 
 int main(int argc, char **argv)
 {
+	puts(MSG_WELCOME);
+	puts(print_menu);
 	while(1){
-		puts(MSG_WELCOME);
-		puts(print_menu);
 
 		/* 'a' is the variable that decides what command the user inputs*/
 		char a;
@@ -31,23 +32,30 @@ int main(int argc, char **argv)
 		scanf("%c", &a);
 
 		if (a == 'p'){
-			
+			/* Has no arguments but is supposed to have: points, level, coiso*/
+			challenge();
 		}
 
 		else if (a == 'm'){
-			
+			puts(print_menu);
+			continue;
 		}
 
 		else if (a == 's'){
+			/* Comentário abaixo para não lixar o código*/
+			/*print_status(level, score, plays);*/
+			continue;
 
 		}
 
 		else if (a == 'q'){
+			puts(MSG_BYE);
 			break;
 		}
 
 		else{
 			puts(MSG_UNKNOWN);
+			continue;
 		}
 
 	}
@@ -64,6 +72,37 @@ int rand_number(const int min, const int max)
 	}
 	int n = abs(max - min) + 1;
 	return (rand() % n) + min;
+}
+
+void challenge (void){
+
+	char game_number[4] = {
+		rand_number(0, 10),
+		rand_number(0, 10),
+		rand_number(0, 10),
+		rand_number(0, 10)
+	};
+
+	for (int j = 0; j < 4; j++){
+
+		char game_number_compare[4];
+		int retard_number = INFINITY;
+		int index;
+
+		for(int i = 0; i < sizeof(game_number); i++){
+			if(game_number[i] < retard_number){
+				retard_number = game_number;
+				index = i;
+			}
+		}
+
+		game_number_compare[j] = retard_number;
+		game_number[index] = INFINITY;
+		
+		// printf para Debugging, verificar se está a comparar e a ordenar os números corretamentes
+		printf("%s", game_number_compare);
+		printf("%s", game_number);
+	}
 }
 
 /* print the game status */
